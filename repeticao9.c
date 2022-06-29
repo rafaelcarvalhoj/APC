3b7c407b18fac4b4c444f18c0e2c2c9ff7454750
rafaelcarvalhoj <105162671+rafaelcarvalhoj@users.noreply.github.com>
2022-06-29 11:27:36 -0300

    Update repeticao9.c

--- repeticao9.c
+++ repeticao9.c
@@ -2,7 +2,6 @@
 
 
 int varificarPrimo(int n){
-    //++n;
     int i,resto;
     int qntDivisores = 0;
     for(i=1;i<=n;i++){
@@ -16,26 +15,32 @@ int varificarPrimo(int n){
 
 int main(){
     int n,numInicial;
-    printf("Digite um numero inteiro: ");
+    printf("Digite um numero inteiro para achar os numeros primos antes e depois dele.: ");
     scanf("%d",&n);
     numInicial = n;
     //varificar proximo primo maior que N
-    do {
-        varificarPrimo(n);
-        n++;
-    }while (varificarPrimo(n) != 2);
-    printf("o proximo numero primo depois de %d eh %d\n",numInicial,n);
-    // verificar proximo primo menor que n
-    n = numInicial;
-    do {
-        if (n == 1){
-            printf("Nao existe numero primo antes de %d\n",numInicial);
-            break;
+    if(n>0){
+        do {
+            varificarPrimo(n);
+            n++;
+        }while (varificarPrimo(n) != 2);
+        printf("o proximo numero primo depois de %d eh %d\n",numInicial,n);
+        // verificar proximo primo menor que n
+        n = numInicial;
+        if(n==1 || n==2){
+            if (n==1){
+                printf("nao existe numero primo antes de 1\n");
+            }else{
+                printf("o proximo numero primo antes de 2 eh 1\n");
+            }
+        }else {
+            do {
+                varificarPrimo(n);
+                n--;
+            }while (varificarPrimo(n) != 2);   
+            printf("o proximo numero primo antes de %d eh %d\n",numInicial,n);
         }
-        varificarPrimo(n);
-        n--;
-    }while (varificarPrimo(n) != 2);
-    if (n!=1){
-        printf("o proximo numero primo antes de %d eh %d\n",numInicial,n);
+    }else{
+        printf("Numero invalido!\n");
     }
 }
