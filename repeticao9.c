eeb2f58190f5f99c37e1eeb0d71f79d61be10f02
rafaelcarvalhoj <105162671+rafaelcarvalhoj@users.noreply.github.com>
2022-06-29 11:51:11 -0300

    Update repeticao9.c

--- repeticao9.c
+++ repeticao9.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-int varificarPrimo(int n){
+int verificarPrimo(int n){
     int i,resto;
     int qntDivisores = 0;
     for(i=1;i<=n;i++){
@@ -18,12 +18,12 @@ int main(){
     printf("Digite um numero inteiro para achar os numeros primos antes e depois dele.: ");
     scanf("%d",&n);
     numInicial = n;
-    //varificar proximo primo maior que N
+    //verificar proximo primo maior que N
     if(n>0){
         do {
-            varificarPrimo(n);
+            verificarPrimo(n);
             n++;
-        }while (varificarPrimo(n) != 2);
+        }while (verificarPrimo(n) != 2);
         printf("o proximo numero primo depois de %d eh %d\n",numInicial,n);
         // verificar proximo primo menor que n
         n = numInicial;
@@ -35,9 +35,9 @@ int main(){
             }
         }else {
             do {
-                varificarPrimo(n);
+                verificarPrimo(n);
                 n--;
-            }while (varificarPrimo(n) != 2);   
+            }while (verificarPrimo(n) != 2);   
             printf("o proximo numero primo antes de %d eh %d\n",numInicial,n);
         }
     }else{
